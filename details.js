688301aa38cf1626ed3122b08d8755c523913bc9
Ricardo Sánchez <ricardosanchez1997@hotmail.com>
2022-12-29 10:04:27 -0500

    task2

---
+++ details.js
@@ -0,0 +1,28 @@
+let contenedor = document.getElementById("contenedorCards")
+let lista = data.events[0]
+let card =document.createElement("div")
+card.classList.add("d-flex")
+card.classList.add("justify-content-evenly")
+card.innerHTML= `<div class="row g-0">
+<div class="col-md-6">
+  <img src="${data.events[0].image}" class="img-fluid rounded-start" alt="Metallica en concierto" style="width:100rem">
+</div>
+<div class="col-md-6 mx-">
+  <div class="card-body">
+    <h5 class="card-title">${data.events[0].name}</h5>
+    <p class="card-text">${data.events[0].description}</p>
+    <p class="card-text">${data.events[0]._id}</p>
+    <p class="card-text">Date: ${data.events[0].date}</p>
+    <p class="card-text">Category: ${data.events[0].category}</p>
+    <p class="card-text">Place: ${data.events[0].place}</p>
+    <p class="card-text">Capacity: ${data.events[0].capacity}</p>
+    <p class="card-text">Assistance: ${data.events[0].assistance}</p>
+    <p class="card-text">Price: ${data.events[0].price}</p>
+  </div>
+</div>
+</div>`
+console.log(card)
+contenedor.appendChild(card)
+
+
+crearCard(lista, contenedor)
---
+++ upcoming.js
@@ -0,0 +1,25 @@
+
+let contenedor = document.getElementById("contenedorCards");
+let lista = data.events
+
+function crearCard(array, where){
+    let card = document.createElement("div")
+    card.classList.add("d-flex")
+    card.classList.add("justify-content-evenly")
+    card.classList.add("flex-wrap")
+    for (let contador of lista){
+        if(data.currentDate < contador.date){
+            card.innerHTML +=`<div class="card shadow p-3 mb-5 bg-body rounded" style="width: 18rem;">
+            <img src="${contador.image}" class="card-img-top" alt="Imagen relacionada">
+            <div class="card-body d-flex flex-column justify-content-between">
+            <h5 class="card-title">${contador.name}</h5>
+            <p class="card-text">${contador.description}</p>
+            <p class="card-text">${contador.date}</p>
+            <a href="#" class="detailsButton">Details</a>
+            </div>
+            </div>`
+        }
+    }
+    where.appendChild(card)
+  }
+crearCard(data.events, contenedor)
